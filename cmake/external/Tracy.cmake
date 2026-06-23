f18fb41860811aaab138c892f7a9eb1a27ee1629
Cem GEÇGEL <gecgelcem@outlook.com>
2026-06-24 00:42:34 +0300

    fix(Tracy): not linking as a library

--- cmake/external/Tracy.cmake
+++ cmake/external/Tracy.cmake
@@ -5,3 +5,20 @@ add_subdirectory(
     "${CMAKE_SOURCE_DIR}/downloads/tracy"
     "${CMAKE_BINARY_DIR}/downloads/tracy"
 )
+
+# NOTE: With TRACY_LTO, TracyClient is always an OBJECT target. We link the
+# object files to a wrapper target and install that instead.
+
+add_library(tracy)
+set_target_properties(tracy PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/downloads/tracy"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/downloads/tracy"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/downloads/tracy"
+)
+
+target_link_libraries(tracy PUBLIC Tracy::TracyClient)
+
+install(
+    TARGETS tracy
+    EXPORT EverSdk
+)
